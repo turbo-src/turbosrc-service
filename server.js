25f498a7b73bb08ccd38bf0c967810a33e9836e2
7db9a <repo.7db9a@gmail.com>
2021-11-29 15:38:39 -0500

    Working roll dice example.

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    hello: String
+    getDie(numSides: Int): RandomDie
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// The root provides the top-level API endpoints
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -23,4 +48,4 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running a GraphQL API server at localhost:4000/graphql');
