7e61edd28fe06ce304c89ad43fc6f11872bcd199
7db9a <repo.7db9a@gmail.com>
2021-12-08 21:58:50 -0500

    Send votes to redis.

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ const { createClient } = require('redis');
 
 (async () => {
 
+const client = createClient({
+  url: 'redis://@172.17.0.2:6379'
+  //host: '172.17.0.2',
+  //port: '6379'
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+
 var schema = buildSchema(`
   type PullRequest {
     vote_code: [String]
@@ -91,29 +101,36 @@ var root = {
     return JSON.stringify(pullRequestsDB)
   },
   setVote: (args) => {
-    const pr_id = args.pr_id
-    var exists = false
+    (async () => {
+      const pr_id = args.pr_id
+      var exists = false
 
-    //If pull request doesn't exist, we have to make one to set a vote.
-    var pullRequest = pullRequestsDB[pr_id]
-    if (typeof pullRequest === 'undefined') {
-      newPullRequest(args);
-      const vote_code = args.contributor_id + "%" + args.side
-      pullRequest = [vote_code]
-    }
-      // Prevent duplicate votes by same contributor on same pull request
-    for (var i = 0; i < pullRequest.length; i++) {
-      var vote_codes = pullRequest[i]
-      if (vote_codes.split("%")[0] === args.contributor_id) {
-        exists = true
+      var vote_code = "undefined";
+
+      //If pull request doesn't exist, we have to make one to set a vote.
+      var pullRequest = pullRequestsDB[pr_id]
+      if (typeof pullRequest === 'undefined') {
+        newPullRequest(args);
+        vote_code = args.contributor_id + "%" + args.side
+        pullRequest = [vote_code]
       }
-    }
-    if (exists === false) {
-      const vote_code = args.contributor_id + "%" + args.side
-      pullRequest.push(vote_code)
-    }
+        // Prevent duplicate votes by same contributor on same pull request
+      for (var i = 0; i < pullRequest.length; i++) {
+        const vote_codes = pullRequest[i]
+        if (vote_codes.split("%")[0] === args.contributor_id) {
+          exists = true
+        }
+      }
+      if (exists === false) {
+        vote_code = args.contributor_id + "%" + args.side
+        pullRequest.push(vote_code)
+      }
 
-    // Push to redis here for newVoteStream
+      // Push to redis here for newVoteStream
+      // key = pr_id, value = vote_code
+      await client.set(pr_id, vote_code);
+    })();
+
     return JSON.stringify(pullRequestsDB)
   },
   newPullRequest: (args) => {
@@ -149,18 +166,4 @@ var way = false;
 //}
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
-
-const client = createClient({
-  url: 'redis://@172.17.0.2:6379'
-  //host: '172.17.0.2',
-  //port: '6379'
-});
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-await client.connect();
-
-await client.set('key', 'value');
-const value = await client.get('key');
-console.log(value);
 })();
