c8c1ad40e9cb386dadd9174a4fd5b412d88401f1
7db9a <repo.7db9a@gmail.com>
2022-03-18 17:14:12 -0400

    tests: fail no repo test.

---
+++ testing/integration/noRepo.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const {
+        postCreateRepo,
+        postSetVote,
+        postGetPRvoteStatus,
+        postNewPullRequest
+      } = require('./../../graphQLrequests')
+const { Parser } = require('graphql/language/parser');
+
+var snooze_ms = 1000;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('Create repo', function () {
+    this.timeout(15000);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    before(async () => {
+        //await postNewPullRequest(
+        //    /*owner:*/ "vim",
+        //    /*repo:*/ "vim",
+        //    /*pr_id:*/ "issue_8457",
+        //    /*contributor_id:*/ "7db9a",
+        //    /*side:*/ "yes",
+        //);
+        //await snooze(1500);
+    });
+    describe.only('Check status after creating a repo.', function () {
+      it("Should do something", async () => {
+        await snooze(1500);
+        const status = await postGetPRvoteStatus(
+            /*owner:*/ "vim",
+            /*repo:*/ "vim",
+            /*pr_id:*/ "issue_8457",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+
+        //console.log(status)
+
+        assert.equal(
+            status,
+            "open",
+            "Fail to stay open even the votes are below the quorum"
+        );
+      });
+    });
+});
