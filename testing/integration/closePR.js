2a7ea4e15707f10220536a34e941a82404585c32
7db9a <repo.7db9a@gmail.com>
2022-04-04 17:05:16 -0400

    test: fail close pull request on gh page.

---
+++ testing/integration/closePR.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const { postSetVote,
+        postGetPRvoteStatus,
+        postGetPRvoteYesTotals,
+        postGetPRvoteNoTotals,
+        postCreateRepo,
+        postNewPullRequest,
+        postClosePullRequest
+      } = require('./../../graphQLrequests')
+const { Parser } = require('graphql/language/parser');
+
+var snooze_ms = 1000;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('Pull requests', function () {
+    this.timeout(15000);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    //before(async () => {
+    //    await postCreateRepo(
+    //        /*owner:*/ "vim",
+    //        /*repo:*/ "vim",
+    //        /*pr_id:*/ "issue_4955",
+    //        /*contributor_id:*/ "7db9a",
+    //        /*side:*/ "yes",
+    //    );
+    //});
+    describe.only('Close pull request.', function () {
+      it("Should close pull request.", async () => {
+        await snooze(1500);
+        await postClosePullRequest(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        assert.equal(
+            true,
+            false,
+            "fail to close pull request"
+        );
+
+        //assert.equal(
+        //    statusDuplicatePR,
+        //    "closed",
+        //    "Duplicate pull request."
+        //);
+
+      });
+    });
+});
