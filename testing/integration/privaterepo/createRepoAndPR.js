5f2cccd96a47940270b60d764bee4c6d4259ba66
7db9a <repo.7db9a@gmail.com>
2022-04-05 14:32:14 -0400

    tests: add create repo and pr case.

---
+++ testing/integration/privaterepo/createRepoAndPR.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const {
+        postCreateRepo,
+        postSetVote,
+        postGetPRvoteStatus,
+        postNewPullRequest
+      } = require('../../../graphQLrequests')
+const { Parser } = require('graphql/language/parser');
+
+var snooze_ms = 1000;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('Create repo', function () {
+    this.timeout(15000);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    before(async () => {
+        await postCreateRepo(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        await postNewPullRequest(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+    });
+    describe.only('Check status after creating a repo.', function () {
+      it("Should do something", async () => {
+        await snooze(1500);
+        const status = await postGetPRvoteStatus(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+
+        //console.log(status)
+
+        assert.equal(
+            status,
+            "open",
+            "Fail to stay open even the votes are below the quorum"
+        );
+      });
+    });
+});
