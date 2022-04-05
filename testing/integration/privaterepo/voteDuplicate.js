f1ec0d46244648c2d64ef80d12b92f405d4ca674
7db9a <repo.7db9a@gmail.com>
2022-04-05 15:03:45 -0400

    tests: vote duplicate for private repo.

---
+++ testing/integration/privaterepo/voteDuplicate.js
@@ -0,0 +1,107 @@
+const assert = require('assert');
+const {
+        postCreateRepo,
+        postSetVote,
+        postGetPRvoteStatus,
+        postNewPullRequest
+      } = require('../../../graphQLrequests')
+const { Parser } = require('graphql/language/parser');
+
+var snooze_ms = 1000;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('Vote duplicate with minority stake voter', function () {
+    this.timeout(15000);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    describe('Check status after vote duplicate', function () {
+      it("Should do something", async () => {
+        await postCreateRepo(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        await postNewPullRequest(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        await postSetVote(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        const openStatus = await postGetPRvoteStatus(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        await postSetVote(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        const duplicateStatus = await postGetPRvoteStatus(
+            /*owner:*/ "vim",
+            /*repo:*/ "vim",
+            /*pr_id:*/ "issue_8949",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+
+        // Close vote otherwise other tests on same server instance won't work.
+        // Only one vote round at a time.
+        await snooze(1500);
+        await postSetVote(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+        await snooze(1500);
+        const closeStatus = await postGetPRvoteStatus(
+            /*owner:*/ "turbo-src",
+            /*repo:*/ "testrepo-white",
+            /*pr_id:*/ "issue_1",
+            /*contributor_id:*/ "7db9a",
+            /*side:*/ "yes",
+        );
+
+        //console.log(status)
+        assert.equal(
+            openStatus,
+            "open",
+            "Fail open on initial vote below quorum"
+        );
+
+        assert.equal(
+            duplicateStatus,
+            "open",
+            "Fail keep open even though initial vote below quorum"
+        );
+        assert.equal(
+            closeStatus,
+            "closed",
+            "Fail to close even the votes exceed the quorum"
+        );
+      });
+    });
+});
