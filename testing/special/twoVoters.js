2b72fe7ab4f14f1adfe90103c818b14f0158aeb8
7db9a <repo.7db9a@gmail.com>
2022-05-06 03:39:25 -0400

    tests: fail to assert new pull request database endpoint.

---
+++ testing/special/twoVoters.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const {
+        postCreateRepo,
+        postCreatePullRequest,
+        postSetVote,
+        postGetPRvoteStatus,
+        postNewPullRequest
+      } = require('../../graphQLrequests')
+const { Parser } = require('graphql/language/parser');
+const fsPromises = require('fs').promises;
+
+var snooze_ms = 1500;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function readDBfile(file) {
+    const data = await fsPromises.readFile(file)
+                       .catch((err) => console.error('Failed to read file', err));
+
+    return data
+}
+
+describe('Create new pull request', function () {
+    this.timeout(snooze_ms*12);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    before(async () => {
+    });
+    describe.only('Check that db server and deprecated server output is the same.', function () {
+      it("Should have the same database after creating a new ull request", async () => {
+        const testDBdata = await readDBfile('testing/special/turbo-src-database-create-repo.json')
+        const deprecatedDBdata = await readDBfile('testing/special/turbo-src-test-database-new-pull-request.json')
+
+        assert.equal(
+            "no",
+            //testDBdata.toString(),
+            deprecatedDBdata.toString()
+        )
+      });
+    });
+});
