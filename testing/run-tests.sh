5caaeec3cc2eb6b7e492b1c48d084f49dab29ba8
7db9a <repo.7db9a@gmail.com>
2022-05-10 20:33:34 -0400

    tests: add GithubMakerTools, git push, and build cycle to test script.

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -1,3 +1,19 @@
+# Rebuild and start containers.
+./dev.sh cycle && ./dev.sh start
+
+# Delete testrepo
+docker run -it \
+gihtub-maker-tools \
+-d -r testrepo
+
+# Create testrepo
+docker run -it \
+gihtub-maker-tools \
+-c -r testrepo
+
+# Push code to testrepo on github.
+git -C $HOME/projects/work/testrepo push --all origin
+
 npm test testing/integration/privaterepo/preTestCreatePRs.js && \
 sleep 3 && \
 
