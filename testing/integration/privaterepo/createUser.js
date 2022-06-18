0956ec53dee862d607f1f60e6a3c07b5a06c80a2
7db9a <repo.7db9a@gmail.com>
2022-06-17 21:06:39 -0400

    tests: pass create User.

--- testing/integration/privaterepo/createUser.js
+++ testing/integration/privaterepo/createUser.js
@@ -2,10 +2,7 @@ const assert = require('assert');
 const fsPromises = require('fs').promises;
 const {
         postCreateUser,
-        postCreateRepo,
-        postSetVote,
-        postGetPRvoteStatus,
-        postNewPullRequest
+        postGetContributorName,
       } = require('../../../graphQLrequests')
 const { Parser } = require('graphql/language/parser');
 
@@ -15,27 +12,27 @@ var snooze_ms = 5000
 // throw duplication errors (ie, data races).
 const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+async function getGithubUser() {
+    const data = await fsPromises.readFile('.config.json')
+                       .catch((err) => console.error('Failed to read file', err));
+
+    let json = JSON.parse(data);
+    let user = json.github.user
+    if (user === undefined) {
+      throw new Error("Failed to load Github user " + user);
+
+    } else {
+      console.log("Successfully read Github " + user);
+    }
+
+    return user
+
+}
 
 describe('Create repo', function () {
     this.timeout(snooze_ms*12);
     // Increase mocha(testing framework) time, otherwise tests fails
     before(async () => {
-        async function getGithubUser() {
-            const data = await fsPromises.readFile('.config.json')
-                               .catch((err) => console.error('Failed to read file', err));
-
-            let json = JSON.parse(data);
-            let user = json.github.user
-            if (user === undefined) {
-              throw new Error("Failed to load Github user " + user);
-
-            } else {
-              console.log("Successfully read Github " + user);
-            }
-
-            return user
-
-        }
         const user  = await getGithubUser();
 
         //Gets it from .config.json
@@ -66,24 +63,33 @@ describe('Create repo', function () {
         //);
         //await snooze(snooze_ms);
     });
-    describe.only('Check status after creating a repo.', function () {
+    describe.only('Get contributor name.', function () {
       it("Should do something", async () => {
-        //await snooze(snooze_ms);
-        //const status = await postGetPRvoteStatus(
-        //    /*owner:*/ user,
-        //    /*repo:*/ "demo",
-        //    /*pr_id:*/ "issue_1",
-        //    /*contributor:*/ user,
-        //    /*side:*/ "yes",
-        //);
+        const user  = await getGithubUser();
+        const maryName = await postGetContributorName(
+            /*owner:*/ user,
+            /*repo:*/ "demo",
+            /*pr_id:*/ "issue_4",
+            /*contributor:*/ "mary",
+        );
+        this.timeout(snooze_ms*50);
+        const userName = await postGetContributorName(
+            /*owner:*/ user,
+            /*repo:*/ "demo",
+            /*pr_id:*/ "issue_4",
+            /*contributor:*/ user,
+        );
 
-        ////console.log(status)
-
-        //assert.equal(
-        //    status,
-        //    "open",
-        //    "Fail to stay open even the votes are below the quorum"
-        //);
+        assert.equal(
+            maryName,
+            "mary",
+            "Fail to get contributors's name from namspace db by contributor id."
+        );
+        assert.equal(
+            userName,
+            user,
+            "Fail to get contributors's signature from namspace db by contibutor id."
+        );
       });
     });
 });
