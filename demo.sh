6e3357924aa11bed34faf9d16edd261eed963b16
7db9a <>
2022-07-08 06:18:19 -0400

    chore(demo): update per new docker-compose workflow.

--- demo.sh
+++ demo.sh
@@ -1,19 +1,6 @@
 # Delete fork of demo.
 ./testing/gihtub-maker.sh
 
-
-# Copy overwrite .config to where it's needed.
-# Can't reach file outside of testing dir
-# for some reason.
-\cp .config.json testing/integration/privaterepo/
-
-sleep 5
-
-# Rebuild and start containers.
-#./dev.sh cycle && ./dev.sh start
-
-sleep 5
-
 npm test testing/integration/privaterepo/preTestForkRepo.js
 
 sleep 5
@@ -21,6 +8,14 @@ sleep 5
 npm test testing/integration/privaterepo/preTestCreatePRs.js
 
 sleep 5
+
+docker build -t turbo-src-server:0.0.1 -f docker/dockerfile.server .
+docker-compose -f docker/docker-compose.yml up -d
+
+# Copy overwrite .config to where it's needed.
+# Can't reach file outside of testing dir
+# for some reason.
+\cp .config.json testing/integration/privaterepo/
 
 echo "Launch extension pop-up and login with Github."
 echo "Then tokenize the repo."
@@ -42,3 +37,9 @@ echo "sleep 5"
 echo "npm test testing/integration/privaterepo/semiAutoTestCreatePRs.js"
 echo "sleep 5"
 echo "npm test testing/integration/privaterepo/semiAutoManyVoters.js"
+echo ""
+echo "Please run"
+echo ""
+echo "docker-compose -f docker/docker-compose.yml down"
+echo ""
+echo "When finished"
