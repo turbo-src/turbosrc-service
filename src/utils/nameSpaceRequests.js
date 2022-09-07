36ff26d8389b2e16f1546e6caeefc42564d6397a
7db9a <repo.7db9a@gmail.com>
2022-09-07 16:33:55 -0400

    fix: get contributor signature had wrong arg type.

--- src/utils/nameSpaceRequests.js
+++ src/utils/nameSpaceRequests.js
@@ -52,11 +52,11 @@ var root = {
     const json = JSON.parse(res.text);
     return json.data.getContributorID;
   },
-  postGetContributorSignature: async (owner, repo, pr_id, contributor_name) => {
+  postGetContributorSignature: async (owner, repo, pr_id, contributor_id) => {
     const res = await superagent
       .post(namespaceUrl)
       .send({
-        query: `{ getContributorSignature(owner: "${owner}", repo: "${repo}", pr_id: "${pr_id}", contributor_name: "${contributor_name}") }`,
+        query: `{ getContributorSignature(owner: "${owner}", repo: "${repo}", pr_id: "${pr_id}", contributor_id: "${contributor_id}") }`,
       })
       .set("accept", "json");
     //.end((err, res) => {
