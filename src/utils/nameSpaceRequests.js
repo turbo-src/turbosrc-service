a50435e6f48c57861aeb8981c8c93ff479ed7a91
Jeffrey Wood <jwfirenze@gmail.com>
2022-09-14 12:46:59 -0400

    lint

--- src/utils/nameSpaceRequests.js
+++ src/utils/nameSpaceRequests.js
@@ -1,9 +1,9 @@
-const superagent = require('superagent')
+const superagent = require('superagent');
 
 const namespaceUrl =
   process.env.NODE_ENV === 'fly'
     ? 'https://namespace-db.fly.dev/graphql'
-    : 'http://localhost:4003/graphql'
+    : 'http://localhost:4003/graphql';
 
 var root = {
   postCreateUser: async (
@@ -19,10 +19,10 @@ var root = {
       .send({
         query: `{ createUser(owner: "${owner}", repo: "${repo}", contributor_id: "${contributor_id}", contributor_name: "${contributor_name}", contributor_signature: "${contributor_signature}", token: "${token}") }`,
       })
-      .set('accept', 'json')
+      .set('accept', 'json');
 
-    const json = JSON.parse(res.text)
-    return json.data.createUser
+    const json = JSON.parse(res.text);
+    return json.data.createUser;
   },
   postGetContributorName: async (owner, repo, pr_id, contributor_id) => {
     const res = await superagent
@@ -30,13 +30,13 @@ var root = {
       .send({
         query: `{ getContributorName(owner: "${owner}", repo: "${repo}", pr_id: "${pr_id}", contributor_id: "${contributor_id}") }`,
       })
-      .set('accept', 'json')
+      .set('accept', 'json');
     // .end((err, res) => {
     //   const json = JSON.parse(res.text);
     //   return json.data.getContributorName;
     // });
-    const json = JSON.parse(res.text)
-    return json.data.getContributorName
+    const json = JSON.parse(res.text);
+    return json.data.getContributorName;
   },
   postGetContributorID: async (owner, repo, pr_id, contributor_name) => {
     const res = await superagent
@@ -44,11 +44,11 @@ var root = {
       .send({
         query: `{ getContributorID(owner: "${owner}", repo: "${repo}", pr_id: "${pr_id}", contributor_name: "${contributor_name}") }`,
       })
-      .set('accept', 'json')
+      .set('accept', 'json');
     //.end((err, res) => {
     //});
-    const json = JSON.parse(res.text)
-    return json.data.getContributorID
+    const json = JSON.parse(res.text);
+    return json.data.getContributorID;
   },
   postGetContributorSignature: async (owner, repo, pr_id, contributor_id) => {
     const res = await superagent
@@ -56,11 +56,11 @@ var root = {
       .send({
         query: `{ getContributorSignature(owner: "${owner}", repo: "${repo}", pr_id: "${pr_id}", contributor_id: "${contributor_id}") }`,
       })
-      .set('accept', 'json')
+      .set('accept', 'json');
     //.end((err, res) => {
     //});
-    const json = JSON.parse(res.text)
-    return json.data.getContributorSignature
+    const json = JSON.parse(res.text);
+    return json.data.getContributorSignature;
   },
   getUser: async (contributor_id) => {
     const res = await superagent
@@ -68,10 +68,10 @@ var root = {
       .send({
         query: `{ getUser(contributor_id: "${contributor_id}") {contributor_name, contributor_id, contributor_signature, token}}`,
       })
-      .set('accept', 'json')
-    const json = JSON.parse(res.text)
-    return json.data.getUser
+      .set('accept', 'json');
+    const json = JSON.parse(res.text);
+    return json.data.getUser;
   },
-}
+};
 
-module.exports = root
+module.exports = root;
