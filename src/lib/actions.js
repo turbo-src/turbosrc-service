b106b5a5776e9a12af45b36d48923a22d1a3bdaa
7db9a <repo.7db9a@gmail.com>
2022-09-21 20:44:17 -0400

    refactor: use pull request string state arg when voting.

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -236,7 +236,7 @@ const root = {
     );
 
     // Merge if turborsc pull request status says there are enough votes to merge.
-    if (prVoteStatus.status === 200 && prVoteStatus.type === 2) {
+    if (prVoteStatus.status === 200 && prVoteStatus.state === "merge") {
        console.log(`Github merge (${args.defaultHash}) disabled)`)
       /*resSetVote =*/ //await mergePullRequest(args.owner, args.repo, args.defaultHash)
     } 
