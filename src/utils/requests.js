a23a00e96f26583cf57514dde24b91196be93635
7db9a <repo.7db9a@gmail.com>
2022-10-05 10:15:45 -0400

    feat: pass api token to transfer tokens.

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -272,7 +272,7 @@ var root = {
     console.log(json);
     return json.data.getContributorTokenAmount;
   },
-  postTransferTokens: async (owner, repo, from, to, amount) => {
+  postTransferTokens: async (owner, repo, from, to, amount, token) => {
     superagent
       .post(`${port}/graphql`)
       .send(
@@ -282,7 +282,7 @@ var root = {
         //{ query: '{ getVoteAll(defaultHash: "default") { vote_code } }' }
         //{ query: `{ getVoteEverything }` }
         {
-          query: `{ transferTokens(owner: "${owner}", repo: "${repo}", from: "${from}", to: "${to}", amount: ${amount}) }`,
+          query: `{ transferTokens(owner: "${owner}", repo: "${repo}", from: "${from}", to: "${to}", amount: ${amount}, token: "${token}" ) }`,
         }
         //{ query: '{ setVote(defaultHash: "default" contributorId: "2", side: 1 ) { vote_code }' }
       ) // sends a JSON post body
--- testing/integration/privaterepo/transferTokens.js
+++ testing/integration/privaterepo/transferTokens.js
@@ -11,6 +11,9 @@ const {
         getContributorAddress,
         getGithubContributor,
       } = require('../../../src/utils/config')
+const {
+       getGithubToken,
+      } = require('../../../src/utils/gitHubUtil.js')
 
 async function readDBfile(file) {
     const data = await fsPromises.readFile(file)
@@ -31,6 +34,7 @@ before(async () => {
 });
   it("Should transfer tokens", async () => {
     const contributor_name = await getGithubContributor()
+    const token = await getGithubToken()
     const contributor_id = await postGetContributorID(
         /*owner:*/ contributor_name,
         /*repo:*/ "demo",
@@ -57,6 +61,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x571BD871120767344b4EE3Ec309c74a3D98aAf0B",
         /*amount:*/ 15_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -64,6 +69,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x493D1c854301054e5D0b0bCFE3cfAe893d573dBa",
         /*amount:*/ 10_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -71,6 +77,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x5061EF86EFcF6Ad2fdcefF8FE9E014a1Ca6801c2",
         /*amount:*/ 75_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -78,6 +85,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x232b9E710e897aEb18FEbe410526B987641BaE5f",
         /*amount:*/ 75_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -85,6 +93,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x55f8B1594beB8eA1fD366c0C138B26e70C03a6ec",
         /*amount:*/ 5_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -92,6 +101,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x45dD192B318e2f1d242954E016492BDF9446381e",
         /*amount:*/ 100_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -99,6 +109,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x1d344C9A2Ee5c0a24336dd1A0c5c79ccD50D06C9",
         /*amount:*/ 50_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -106,6 +117,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0xd30Dcb56A4d3EC2dC8591588455A5Da4C3c84eCD",
         /*amount:*/ 10_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -113,6 +125,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x9095B61290249584d9d0447657a03Cf23BF7a325",
         /*amount:*/ 50_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -120,6 +133,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0x1d0798e209A07715765F1486CA64f3D2399aF719",
         /*amount:*/ 75_000,
+        /*token:*/ token
     );
     await postTransferTokens(
         /*owner:*/ contributor_name,
@@ -127,6 +141,7 @@ before(async () => {
         /*from:*/ contributor_id,
         /*to:*/ "0xDB7A25D3B4C5506779bD9f9f1A5AA0DB525Fa6A8",
         /*amount:*/ 999,
+        /*token:*/ token
     );
 
     // Give offchain service a chance to write transfer amounts.
