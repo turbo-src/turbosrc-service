a137eda3161e79f11e97e9533064574f9266fa9b
7db9a <repo.7db9a@gmail.com>
2022-10-12 11:33:18 -0400

    refactor: gh service gets endpoint from config.

--- src/utils/ghServiceRequests.js
+++ src/utils/ghServiceRequests.js
@@ -1,6 +1,6 @@
 const superagent = require("superagent");
 
-const port = "http://localhost:4004";
+const { getServiceEndpoint } = require('./config')
 
 //const port =
 //  process.env.NODE_ENV === "fly"
@@ -13,8 +13,9 @@ var root = {
     issue_id,
     tsrc_id
   ) => {
+  const endpoint = await getServiceEndpoint("gh")
   const res = await superagent
-      .post(`${port}/graphql`)
+      .post(endpoint)
       .send({
         query: `{ createIssue(repo: "${repo}", issue_id: "${issue_id}", tsrc_id: "${tsrc_id}") }`,
       })
@@ -23,8 +24,9 @@ var root = {
       return json.data.createIssue;
   },
   postGetIssueID: async (repo, tsrc_id) => {
+    const endpoint = await getServiceEndpoint("gh")
     const res = await superagent
-      .post(`${port}/graphql`)
+      .post(endpoint)
       .send({
         query: `{ getIssueID(repo: "${repo}", tsrc_id: "${tsrc_id}") }`,
       })
@@ -33,8 +35,9 @@ var root = {
     return json.data.getIssueID;
   },
   postGetTsrcID: async (repo, issue_id) => {
+    const endpoint = await getServiceEndpoint("gh")
     const res = await superagent
-      .post(`${port}/graphql`)
+      .post(endpoint)
       .send({
         query: `{ getTsrcID(repo: "${repo}", issue_id: "${issue_id}") }`,
       })
