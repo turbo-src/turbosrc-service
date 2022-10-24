d6fc3c07125381abb93c27c02e7e39df09f27064
7db9a <repo.7db9a@gmail.com>
2022-10-24 16:51:47 -0400

    tests: fix test that create GH pull requests for demo test.

--- testing/integration/privaterepo/preTestCreatePRs.js
+++ testing/integration/privaterepo/preTestCreatePRs.js
@@ -2,12 +2,13 @@ const assert = require('assert');
 const fsPromises = require('fs').promises;
 const {
         postCreateRepo,
-        postCreatePullRequest,
         postSetVote,
-        postNewPullRequest,
         postGetContributorID,
         postGetContributorName,
       } = require('../../../src/utils/requests')
+const {
+	createPullRequest,
+      } = require('../../../src/utils/gitHubUtil')
 const { Parser } = require('graphql/language/parser');
 const {
         getContributorAddress,
@@ -29,51 +30,51 @@ describe('Create repo and GH pull request', function () {
       it("Should do something", async () => {
         const contributor_name = await getGithubContributor()
         await snooze(snooze_ms);
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest1",
-            /*defaultHash:*/ "defaultHash1",
+            /*issue:*/ "issue_1",
             /*title:*/ "refactor(lsp): remove redundant client cleanup",
         );
         await snooze(snooze_ms);
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest2",
-            /*defaultHash:*/ "defaultHash2",
+            /*issue:*/ "issue_2",
             /*title:*/ "refactor(uncrustify): set maximum number of consecutive newlines"
         );
         await snooze(snooze_ms);
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest3",
-            /*defaultHash:*/ "defaultHash3",
+            /*issue:*/ "issue_3",
             /*title:*/ "ci(mingw): only enable -municode for MinGW",
         );
         await snooze(snooze_ms);
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest4",
-            /*defaultHash:*/ "defaultHash4",
+            /*issue:*/ "issue_4",
             /*title:*/ "docs: add missing termdebug docs from Vim runtime updates",
         );
 
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest5",
-            /*defaultHash:*/ "defaultHash5",
+            /*issue:*/ "issue_5",
             /*title:*/ "refactor: missing parenthesis may cause unexpected problems",
         );
 
-        await postCreatePullRequest(
+        await createPullRequest(
             /*owner:*/ contributor_name,
             /*repo:*/ "demo",
             /*fork_branch:*/ "pullRequest6",
-            /*defaultHash:*/ "defaultHash6",
+            /*issue:*/ "issue_6",
             /*title:*/ "refactor(normal): convert function comments to doxygen format",
         );
         assert.equal(
