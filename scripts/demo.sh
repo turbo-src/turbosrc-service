275c0177071cba6956efe3146c0655298e5a70f6
7db9a <>
2023-02-22 06:20:09 -0500

    fix(scripts): saving progress.

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,8 +1,8 @@
-#docker build -t turbo-src-server:0.0.1 -f docker/dockerfile.server .
-#docker-compose -f docker/docker-compose.yml up -d
-# Copy overwrite .config to where it's needed.
-# Can't reach file outside of testing dir
-# for some reason.
+##docker build -t turbo-src-server:0.0.1 -f docker/dockerfile.server .
+##docker-compose -f docker/docker-compose.yml up -d
+## Copy overwrite .config to where it's needed.
+## Can't reach file outside of testing dir
+## for some reason.
 \cp .config.json testing/integration/privaterepo/
 
 # Delete fork of demo.
@@ -16,32 +16,28 @@ docker run -it fork-repo node fork-repo.js "turbo-src/demo"
 sleep 5
 
 # Create pull requests.
+# docker run -it create-pull-request python create_pull_request.py owner repo base head title body
+
 docker run -it create-pull-requests \
 python create_pull_requests.py "7db9a" "demo" "master" "pullRequest1" "refactor(lsp): remove redundant client cleanup" "auto pull request"
 
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines"
-#
-#
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW"
-#
-#
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "docs: add missing termdebug docs from Vim runtime updates"
-#
-#
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest4" "refactor: missing parenthesis may cause unexpected problems"
-#
-#
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems"
-#
-#
-#docker run -it create-pull-requests \
-#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format"
-#
+docker run -it create-pull-requests \
+python create_pull_requests.py "7db9a" "demo" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines" "auto pull request"
+
+docker run -it create-pull-requests \
+python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW" "auto pull request"
+
+
+docker run -it create-pull-requests \
+python create_pull_requests.py "7db9a" "demo" "master" "pullRequest4" "docs: add missing termdebug docs from Vim runtime updates" "auto pull request"
+
+docker run -it create-pull-requests \
+python create_pull_requests.py "7db9a" "demo" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems" "auto pull request"
+
+
+docker run -it create-pull-requests \
+python create_pull_requests.py "7db9a" "demo" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format" "auto pull request"
+
 #sleep 5
 #
 #npm test testing/integration/privaterepo/createUser.js
