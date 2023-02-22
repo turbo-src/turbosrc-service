2260fd243ce8f1c7e0a6433fc99ab923acc31510
7db9a <>
2023-02-22 05:50:18 -0500

    Saving progress.

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -19,45 +19,45 @@ sleep 5
 docker run -it create-pull-requests \
 python create_pull_requests.py "7db9a" "demo" "master" "pullRequest1" "refactor(lsp): remove redundant client cleanup" "auto pull request"
 
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "docs: add missing termdebug docs from Vim runtime updates"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest4" "refactor: missing parenthesis may cause unexpected problems"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format"
-
-sleep 5
-
-npm test testing/integration/privaterepo/createUser.js
-sleep 5
-npm test testing/integration/privaterepo/transferTokens.js
-sleep 5
-npm test testing/integration/privaterepo/twoVoters.js
-sleep 5
-npm test testing/integration/privaterepo/singleMajorityVoter.js
-sleep 5
-npm test testing/integration/privaterepo/duplicateVote.js
-sleep 5
-npm test testing/integration/privaterepo/manyVoters.js
-sleep 5
-npm test testing/integration/privaterepo/semiAutoTestCreatePRs.js
-sleep 5
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines"
+#
+#
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW"
+#
+#
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "docs: add missing termdebug docs from Vim runtime updates"
+#
+#
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest4" "refactor: missing parenthesis may cause unexpected problems"
+#
+#
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems"
+#
+#
+#docker run -it create-pull-requests \
+#python create_pull_requests.py "7db9a" "demo" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format"
+#
+#sleep 5
+#
+#npm test testing/integration/privaterepo/createUser.js
+#sleep 5
+#npm test testing/integration/privaterepo/transferTokens.js
+#sleep 5
+#npm test testing/integration/privaterepo/twoVoters.js
+#sleep 5
+#npm test testing/integration/privaterepo/singleMajorityVoter.js
+#sleep 5
+#npm test testing/integration/privaterepo/duplicateVote.js
+#sleep 5
+#npm test testing/integration/privaterepo/manyVoters.js
+#sleep 5
+#npm test testing/integration/privaterepo/semiAutoTestCreatePRs.js
+#sleep 5
 
 echo "Please run the final test that requires to actually vote on the extension to merge or close."
 echo ""
