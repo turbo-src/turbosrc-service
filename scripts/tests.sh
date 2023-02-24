9fd71df7693abb5a4b3e2108ffe9a945ce1e6827
7db9a <>
2023-02-24 11:12:04 -0500

    scripts(tsrc-dev): script runs from cmd line with args.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,69 +1,80 @@
-##docker build -t turbo-src-server:0.0.1 -f docker/dockerfile.server .
-##docker-compose -f docker/docker-compose.yml up -d
-## Copy overwrite .config to where it's needed.
-## Can't reach file outside of testing dir
-## for some reason.
-\cp .config.json testing/integration/privaterepo/
+#!/bin/bash
 
-# Delete fork of demo.
-docker run -it \
-gihtub-maker-tools \
--d -r demo
+function delete_fork() {
+  REPO=$1
+  docker run -it gihtub-maker-tools -d -r $REPO
+}
 
-# Fork the demo.
-docker run -it fork-repo node fork-repo.js "turbo-src/demo"
+function fork_repo() {
+  REPO=$1
+  docker run -it fork-repo node fork-repo.js "turbo-src/$REPO"
+}
 
-sleep 5
+function create_pull_request() {
+  REPO=$1
+  docker run -it create-pull-requests python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest1" "refactor(lsp): remove redundant client cleanup" "auto pull request"
+  docker run -it create-pull-requests \
+  python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines" "auto pull request"
+  docker run -it create-pull-requests \
+  python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW" "auto pull request"
+  docker run -it create-pull-requests \
+  python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest4" "docs: add missing termdebug docs from Vim runtime updates" "auto pull request"
+  docker run -it create-pull-requests \
+  python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems" "auto pull request"
+  docker run -it create-pull-requests \
+  python create_pull_requests.py "7db9a" "$REPO" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format" "auto pull request"
+}
 
-# Create pull requests.
-# docker run -it create-pull-request python create_pull_request.py owner repo base head title body
+function run_tests() {
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/createUser.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/createUser.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/createRepo.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/transferTokens.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/twoVoters.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/singleMajorityVoter.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/duplicateVote.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/manyVoters.js && sleep 5
+  npm test --prefix turbosrc-service/ testing/integration/privaterepo/semiAutoManyVoters.js && sleep 5
+}
 
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest1" "refactor(lsp): remove redundant client cleanup" "auto pull request"
+function execute_all_except_tests() {
+  delete_fork $REPO
+  fork_repo $REPO
+  create_pull_request $REPO
+}
 
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest2" "refactor(uncrustify): set maximum number of consecutive newlines" "auto pull request"
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <username> <repository> [delete_fork|fork_repo|create_pull_request|run_tests|execute_all|execute_all_except_tests]"
+  exit 1
+fi
 
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest3" "ci(mingw): only enable -municode for MinGW" "auto pull request"
+USERNAME=$1
+REPO=$2
 
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest4" "docs: add missing termdebug docs from Vim runtime updates" "auto pull request"
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest5" "refactor: missing parenthesis may cause unexpected problems" "auto pull request"
-
-
-docker run -it create-pull-requests \
-python create_pull_requests.py "7db9a" "demo" "master" "pullRequest6" "refactor(normal): convert function comments to doxygen format" "auto pull request"
-
-sleep 5
-
-npm test testing/integration/privaterepo/createUser.js
-sleep 5
-npm test testing/integration/privaterepo/createRepo.js
-sleep 5
-npm test testing/integration/privaterepo/transferTokens.js
-sleep 5
-npm test testing/integration/privaterepo/twoVoters.js
-sleep 5
-npm test testing/integration/privaterepo/singleMajorityVoter.js
-sleep 5
-npm test testing/integration/privaterepo/duplicateVote.js
-sleep 5
-npm test testing/integration/privaterepo/manyVoters.js
-sleep 5
-npm test testing/integration/privaterepo/semiAutoManyVoters.js
-sleep 5
-
-echo "Please go to your browser and naviagate to the demo page in your github."
-echo "Then please vote on pull request #6 to merge or close it."
-
-
-#echo ""
-#echo "Please run"
-#echo ""
-#echo "docker-compose -f docker/docker-compose.yml down"
-#echo ""
-#echo "When finished"
+case "$3" in
+  "delete_fork")
+    delete_fork $REPO
+    ;;
+  "fork_repo")
+    fork_repo $REPO
+    ;;
+  "create_pull_request")
+    create_pull_request $REPO
+    ;;
+  "run_tests")
+    run_tests
+    ;;
+  "execute_all")
+    delete_fork $REPO
+    fork_repo $REPO
+    create_pull_request $REPO
+    run_tests
+    ;;
+  "execute_all_except_tests")
+    execute_all_except_tests
+    ;;
+  *)
+    echo "Usage: $0 <username> <repository> [delete_fork|fork_repo|create_pull_request|run_tests|execute_all|execute_all_except_tests]"
+    exit 1
+    ;;
+esac
