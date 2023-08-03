7a93f5cbe3322ced172d308e59c0c398b97ef8d5
7db9a <>
2023-08-03 04:08:16 -0400

    Fix socket connection.

--- socketConfig.js
+++ socketConfig.js
@@ -1,14 +1,19 @@
 const { io } = require("socket.io-client");
 
 const socket = io("http://turbosrc-egress-router:4007/", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd"
-  }
-}
-  );
+  path: '/vote-client/socket.io',
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        'my-custom-header': 'abcd'
+      }
+    }
+  },
+  transports: ['websocket'], // <---- Add this line
+  secure: false,  // ensure secure connection
+});
 
 socket.on("connect", () => console.log('socket id:', socket.id))
 socket.on("disconnect", () => console.log('socket disconnected'))
 
-  module.exports = { socket }
+module.exports = { socket }
