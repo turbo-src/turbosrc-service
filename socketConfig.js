fc2505b9cdc915629c47c9248e36286816c1f518
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2023-09-23 11:37:23 -0400

    socketConfig.js updated

--- socketConfig.js
+++ socketConfig.js
@@ -1,14 +1,34 @@
 const { io } = require("socket.io-client");
 
+/* 
+   This file mimics the client side socket for testing. In production, chrome-extension is the client
+   and egress-router/service is the server. In the tests, there is no client, so we have the below to simulate
+   a client connecting and test that our socket is working.
+*/
+
 const socket = io("http://turbosrc-egress-router:4007/", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd"
-  }
-}
-  );
+	path: "/vote-client/socket.io",
+	transportOptions: {
+		polling: {
+			extraHeaders: {
+				"my-custom-header": "abcd",
+			},
+		},
+	},
+	transports: ["websocket"], // <---- Add this line
+	secure: false, // ensure secure connection
+});
 
-socket.on("connect", () => console.log('socket id:', socket.id))
-socket.on("disconnect", () => console.log('socket disconnected'))
+socket.on("connect", () => {
+	console.log("Socket connection established");
+});
 
-  module.exports = { socket }
+socket.on("connect_error", (error) => {
+	console.error("Socket connection error:", error);
+});
+
+socket.on("disconnect", (reason) => {
+	console.log("Socket disconnected:", reason);
+});
+
+module.exports = { socket };
