e832513cbce0d38140c8f16e5d2cf2681509191a
7db9a <>
2023-11-07 15:25:19 -0500

    Decrypt with payload w/nonce (aka IV) prepended.

--- sharedSecret.js
+++ sharedSecret.js
@@ -1,39 +1,31 @@
 const crypto = require('crypto');
 const secp256k1 = require('secp256k1');
-const ethUtil = require('ethereumjs-util');
 
-// Your private key (must be 32 bytes hex string without 0x prefix)
-const clientEthereumPrivateKeyHex = '..';
+// Server's private key (must be 32 bytes hex string without 0x prefix)
+const serverPrivateKeyHex = '...';
 
-// Ensure the private key is a Buffer
-const clientEthereumPrivateKeyBuffer = Buffer.from(clientEthereumPrivateKeyHex, 'hex');
+// Convert the server's private key to a Buffer
+const serverPrivateKeyBuffer = Buffer.from(serverPrivateKeyHex, 'hex');
 
-// Assume you've received the other party's public key as a hex string
-const serverEthereumPublicKeyHex = '..'
-const serverEthereumPublicKeyBuffer = Buffer.from(serverEthereumPublicKeyHex, 'hex');
+// Client's public key received by the server (hex string)
+const clientPublicKeyHex = '...';
+const clientPublicKeyBuffer = Buffer.from(clientPublicKeyHex, 'hex');
 
-// Generate the shared secret
-const sharedSecret = secp256k1.ecdh(serverEthereumPublicKeyBuffer, clientEthereumPrivateKeyBuffer);
-
-console.log('Shared secret:', sharedSecret.toString('hex'));
+// Server generates the shared secret using its private key and client's public key
+const sharedSecret = secp256k1.ecdh(clientPublicKeyBuffer, serverPrivateKeyBuffer);
 
 // Create a SHA-256 hash of the shared secret to use as the key for AES-256
 const key = crypto.createHash('sha256').update(sharedSecret).digest();
 
-// The initialization vector should be random and unique for each encryption
-const iv = crypto.randomBytes(16);
+const payload = '...'; // The hex string received that includes the IV and encrypted data
 
-// Encrypt a message
-const message = 'Hello, World!';
-const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-let encrypted = cipher.update(message, 'utf8', 'hex');
-encrypted += cipher.final('hex');
+// Extract the IV from the beginning of the payload
+const iv = Buffer.from(payload.substring(0, 32), 'hex'); // IV is 16 bytes, hence 32 hex characters
+const encryptedMessage = payload.substring(32);
 
-console.log('Encrypted message:', encrypted);
-
-// To decrypt the message later on:
+// Perform decryption using the extracted IV
 const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+let decrypted = decipher.update(encryptedMessage, 'hex', 'utf8');
 decrypted += decipher.final('utf8');
 
 console.log('Decrypted message:', decrypted);
