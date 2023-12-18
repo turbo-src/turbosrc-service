3f921c32b3509c1f02f64d7d4984ae58b091c28a
7db9a <>
2023-12-18 15:47:17 -0500

    Saving progress.

---
+++ testing/integration/privaterepo/conflictResolution.js
@@ -0,0 +1,120 @@
+const assert = require('assert');
+const fsPromises = require('fs').promises;
+const { postSetVote,
+        postGetPullRequest,
+        postGetPRvoteYesTotals,
+        postGetPRvoteNoTotals,
+        postCreateRepo,
+        postNewPullRequest,
+        postGetContributorID,
+        postGetContributorName,
+	    getGitHubPullRequest,
+        getNameSpaceRepo
+      } = require('../../../src/utils/requests')
+const { Parser } = require('graphql/language/parser');
+const {
+        getContributorAddress,
+        getGithubContributor,
+      } = require('../../../src/utils/config')
+const {
+       getGithubToken,
+      } = require('../../../src/utils/gitHubUtil.js')
+      const {socket} = require("../../../socketConfig")
+
+var snooze_ms = 1500;
+
+// We call this at the top of each test case, otherwise nodeosd could
+// throw duplication errors (ie, data races).
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+describe('Vote on conflict resovled pull request.', function () {
+    this.timeout(snooze_ms*12);
+    // Increase mocha(testing framework) time, otherwise tests fails
+    before(async () => {
+        //const contributor_name = await getGithubContributor()
+
+        //const tsrctester1ID = await postGetContributorID(
+        //    /*owner:*/ contributor_name,
+        //    /*repo:*/ "demo",
+        //    /*defaultHash:*/ "issue_6",
+        //    /*contributor_name:*/ "tsrctester1",
+        //);
+
+        //const testerTokenA = await getGithubToken("a")
+        //await snooze(snooze_ms);
+
+        //const { repoID } = await getNameSpaceRepo(`${contributor_name}/demo`);
+
+        //await postSetVote(
+        //    /*owner:*/ contributor_name,
+        //    /*repo: */ repoID,
+        //    /*defaultHash:*/ "issue_6",
+        //    /*childDefaultHash:*/ "issue_6",
+	    /*m//ergeable:*/ true,
+        //    /*contributor_id:*/ tsrctester1ID,
+        //    /*side:*/ "yes",
+	    /*t//oken:*/ testerTokenA
+        //);
+        //socket.emit('vote cast', contributor_name, repoID, "issue_6")
+        //socket.disconnect()
+
+    });
+    describe.only('A single majority voter votes on a conflict resolved PR.', function () {
+      it("Should close vote and then merge.", async () => {
+        //const contributor_name = await getGithubContributor()
+        //const tsrctester1ID = await postGetContributorID(
+        //    /*owner:*/ contributor_name,
+        //    /*repo:*/ "demo",
+        //    /*defaultHash:*/ "issue_6",
+        //    /*contributor_name:*/ "tsrctester1",
+        //);
+        //const { repoID } = await getNameSpaceRepo(`${contributor_name}/demo`);
+
+        //await snooze(snooze_ms);
+
+        //const mergeStatus = await postGetPullRequest(
+        //    /*owner:*/ contributor_name,
+        //    /*repo: */ repoID,
+        //    /*defaultHash:*/ "issue_6",
+        //    /*contributor_id:*/ tsrctester1ID,
+        //    /*side:*/ "yes",
+        //);
+        //await snooze(snooze_ms);
+        //const voteYesTotals = await postGetPRvoteYesTotals(
+        //    /*owner:*/ contributor_name,
+        //    /*repo: */ repoID,
+        //    /*defaultHash:*/ "issue_6",
+        //    /*contributor_id:*/ tsrctester1ID,
+        //    /*side:*/ "yes",
+        //);
+        //await snooze(snooze_ms);
+        //const voteNoTotals = await postGetPRvoteNoTotals(
+        //    /*owner:*/ contributor_name,
+        //    /*repo: */ repoID,
+        //    /*defaultHash:*/ "issue_6",
+        //    /*contributor_id:*/ tsrctester1ID,
+        //    /*side:*/ "yes",
+        //);
+
+        assert.equal(true, false, "fail assertion")
+
+        //assert.deepEqual(
+        //  mergeStatus,
+        // { status: 200, state: "merge", "mergeableCodeHost": true, repo_id: repoID,  fork_branch: "pullRequest2", "childDefaultHash": "8fff757c05b091712c8f170673b74c19134c34c4", "defaultHash": "8fff757c05b091712c8f170673b74c19134c34c4" },
+        // { status: 200, type: 2 },
+        //  "Fail to merge even though it was voted in."
+        //);
+
+        //assert.equal(
+        //    voteYesTotals,
+        //    "500001",
+        //    "Fail to add votes yes."
+        //);
+        //assert.equal(
+        //    voteNoTotals,
+        //    "0",
+        //    "Fail to zero out voteNoTotals after vote close."
+        //);
+      });
+    });
+});
